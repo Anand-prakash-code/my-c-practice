d2c45d95a2ea360f97501553bab42dd85fea19f5
Anand-prakash-code <prakashanandjh@gmail.com>
2026-08-22 20:32:54 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{ int day;// like 1 for monday and 7 for sunday 
+printf("enter day 1-7\n ");
+ scanf("%d",&day);
+
+switch(day)
+{ case 1: printf("monday\n");
+  break;  
+ case 2 :printf("tuesday");
+break;}
+
+return 0;}
