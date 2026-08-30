b7f59cee24c82132f8c1725ebe1e2fe484778c38
Anand prakash <prakashanandjh@gmail.com>
2026-08-30 10:54:13 +0530

    Add files via upload

---
+++ recursion-q-pra-3.c
@@ -0,0 +1,21 @@
+ //write a fxn to calculate % of student from marks in science ,math and sanskrit
+
+ #include<stdio.h>
+
+ float PercentMarks (float x,float y, float z);
+
+ int main()
+{float x,y,z,m;
+printf("enter your marks in math ,sanskrit,and science\n ");
+scanf("%f,%f,%f",&x,&y,&z);
+m = PercentMarks (x,y,z);
+printf("so the percentage is %f\n",m);
+return 0;
+}
+
+float PercentMarks (float x,float y,float z)
+{
+    return  (x+y+z)/300*100 ;
+}
+
+
