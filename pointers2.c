d744ea7ae1a8e34205d34d2a9ae1fbd0c3dedc10
Anand prakash <prakashanandjh@gmail.com>
2026-09-07 07:32:02 +0530

    Update pointers2.c

--- pointers2.c
+++ pointers2.c
@@ -2,7 +2,7 @@
 
  int main ()
  { int age = 22;
-   int *ptr = &age;
+   int *ptr = &age; // *ptr mean the value stored at the memory address that ptr is pointing to
    printf("%u\n",&age); // here &age mean address of age =642230
    printf(" %u\n",&ptr); // here &ptr mean address of ptr= 6422296
    printf(" %u\n",*ptr); // it gives value of that address = 22
