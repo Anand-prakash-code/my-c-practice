3b594466ed764769c0deae7b317af237c15d225a
Anand prakash <prakashanandjh@gmail.com>
2026-09-09 19:31:03 +0530

    Update array3Q.c

--- array3Q.c
+++ array3Q.c
@@ -4,7 +4,7 @@
     int arrD []={ 1,2,3,4,5};
     int  *arr = &arrD[0] ;
     printf (" we got %d",*(arr+2));
-
+    printf (" we got %d", *(arr +5)): //will get a random garbage value stored at memory address like i got 6422280
 
     return 0;
  }
