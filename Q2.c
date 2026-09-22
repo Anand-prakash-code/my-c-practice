1a049c7de750b1b51cd3daa67a5640a8f76a5dc2
Anand prakash <prakashanandjh@gmail.com>
2026-09-22 22:37:48 +0530

    Implement number to word conversion and even/odd check

---
+++ Q2.c
@@ -0,0 +1,20 @@
+// not modify  use const
+const char *words[] = {"zeor","one","two","three","four","five","six","seven","eight","nine"};
+
+for(int i = a; i<=b; i++){
+
+    if(i<=9){
+        printf("%s\n",words[i]);
+    }else{
+        // even or odd
+        if(i % 2 == 0){
+            printf("even\n");
+        }else{
+            printf("odd\n");
+        }
+
+    }
+}
+
+
+return 0;
